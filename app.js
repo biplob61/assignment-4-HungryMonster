b53d062cfc7fd3387ca1526ce1e7f3c4ac303456
biplob61 <bhbiplob61@gmail.com>
2021-02-07 17:28:29 +0600

    updated food list

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 function inputHandler(){
     const searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click",function(){
-    //console.log("click marse");
     const inputArea = document.getElementById("input-area").value;
-    console.log(inputArea);
+
 })
 }
+
+
 //meallist handler
 fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
 .then(response => response.json())
@@ -25,5 +26,20 @@ const displayMeal = mealNames=>{
     }
 }
 
+// function displayMealHandler(){
+//     const displayMeal = mealNames=>{
+//         const mealList = mealNames.meals
+//         const div = document.getElementById("mealNames");
+//         for (let i = 0; i < mealList.length; i++) {
+//             const meal = mealList[i];
+//             const mealDiv = document.createElement('div');
+//             mealDiv.className = 'mealContainer';
+//             mealDiv.innerText = meal.strMeal;
+//             div.appendChild(mealDiv);
+            
+//         }
+//     }
+//     return displayMealHandler
+// }
 
 
